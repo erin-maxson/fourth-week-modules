2f4fe12bd0de4905d896439d6140bb50bc22a02c
Erin Maxson <103834510+erin-maxson@users.noreply.github.com>
2022-05-16 18:14:19 -0500

    Adding fourth week files

---
+++ 02-Stu_Window-Object/Unsolved/script.js
@@ -0,0 +1,16 @@
+// TODO: What will the following line of code log?
+// This will log the window object
+console.log(window);
+
+// TODO: What will the following line of code log?
+// Will log the window document
+console.log(window.document);
+
+// TODO: What will the following line of code log?
+// Will log element/class of the document 
+console.log(document.documentElement);
+
+// TODO: What will the following line of code log?
+// Will log the head of the document
+console.log(document.head);
+

8c5a52e871f3fc9f7c015b26c86fc152703848b3
erin-maxson <erin@cohatch.com>
2022-05-18 17:43:57 -0700

    keydown and keyup events still need to understand

--- 16-Stu_Keyboard-Events/Unsolved/assets/js/script.js
+++ 16-Stu_Keyboard-Events/Unsolved/assets/js/script.js
@@ -1,5 +1,15 @@
+var code = document.getElementById('code');
+var codeID = document.querySelector("#code");
+var keyID = document.querySelector("#key");
+var statusID = document.querySelector("status");
+
 function keydownAction(event) {
   // TODO: Complete keydown function
+console.log(event);
+codeID.innerHTML = event.key;
+keyID.innerHTML = event.code;
+statusID.innerHTML = "KEYDOWN Event";
+
 }
 
 function keyupAction() {
@@ -9,3 +19,5 @@ function keyupAction() {
 document.addEventListener("keyup", keyupAction);
 // TODO: Add Event Listener for "keydown" event
 
+
+document.addEventListener("keydown", keydownAction);

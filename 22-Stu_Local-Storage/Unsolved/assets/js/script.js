dc0b1ac51bac53782e7def8f5600694d2ce7bd35
erin-maxson <erin@cohatch.com>
2022-05-21 08:28:02 -0700

    updating local storage activity

--- 22-Stu_Local-Storage/Unsolved/assets/js/script.js
+++ 22-Stu_Local-Storage/Unsolved/assets/js/script.js
@@ -14,9 +14,14 @@ function displayMessage(type, message) {
 
 function renderLastRegistered() {
   // TODO: Retrieve the last email and password and render it to the page
+  var email = localStorage.getItem("email");
+  var password = localStorage.getItem("password");
+  userEmailSpan.textContent = email;
+  userPasswordSpan.textContent = password;
 }
 
-signUpButton.addEventListener("click", function(event) {
+
+signUpButton.addEventListener("click", function (event) {
   event.preventDefault();
 
   var email = document.querySelector("#email").value;
@@ -29,6 +34,8 @@ signUpButton.addEventListener("click", function(event) {
   } else {
     displayMessage("success", "Registered successfully");
 
-  // TODO: Save email and password to localStorage and render the last registered user
+    // TODO: Save email and password to localStorage and render the last registered user
+    localStorage.setItem("email", email);
+    localStorage.setItem("password", password);
   }
 });

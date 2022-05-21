2717d54bd1e16a97b25dbb444feca9e72030e1d8
erin-maxson <erin@cohatch.com>
2022-05-21 10:18:40 -0700

    adding variables to mini project

--- 26-Stu_Local-Storage-Todos/Unsolved/assets/js/script.js
+++ 26-Stu_Local-Storage-Todos/Unsolved/assets/js/script.js
@@ -6,12 +6,16 @@ var todoCountSpan = document.querySelector("#todo-count");
 var todos = [];
 
 // TODO: What is the purpose of this function?
+//Provide a place for you to enter the to-dos into the function on the HTML/frontend side
 function renderTodos() {
   // TODO: Describe the functionality of the following two lines of code.
+  // Allows you to edit the to do list from the innerHTML
   todoList.innerHTML = "";
+  //adds the to do list items by number (1, 2, 3, etc.,) into the to do counter
   todoCountSpan.textContent = todos.length;
   
   // TODO: Describe the functionality of the following `for` loop.
+  // adds as many to do list items as entered
   for (var i = 0; i < todos.length; i++) {
     var todo = todos[i];
 
@@ -28,26 +32,33 @@ function renderTodos() {
 }
 
 // TODO: What is the purpose of the following function?
+// this function fires as the page is loaded
 function init() {
   // TODO: What is the purpose of the following line of code?
+  //parses the to dos into individual objects/data for local storage
   var storedTodos = JSON.parse(localStorage.getItem("todos"));
   // TODO: Describe the functionality of the following `if` statement.
+  //adds to dos to the stored file if there isn't any
   if (storedTodos !== null) {
     todos = storedTodos;
   }
   // TODO: Describe the purpose of the following line of code.
+  // runs the to dos through the function/returns the to dos on the page
   renderTodos();
 }
 
 function storeTodos() {
   // TODO: Describe the purpose of the following line of code.
+  // pulls the to do items into one string
   localStorage.setItem("todos", JSON.stringify(todos));
 }
 // TODO: Describe the purpose of the following line of code.
+// Adds an event listener when someone clicks submit to activate an event
 todoForm.addEventListener("submit", function(event) {
   event.preventDefault();
   var todoText = todoInput.value.trim();
   // TODO: Describe the functionality of the following `if` statement.
+  // 
   if (todoText === "") {
     return;
   }

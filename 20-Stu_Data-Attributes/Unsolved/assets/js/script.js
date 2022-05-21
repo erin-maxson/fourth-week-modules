942a3cba42d22624ef8d7f01ce861362ece90383
erin-maxson <erin@cohatch.com>
2022-05-21 07:58:46 -0700

    working on third day activities

--- 20-Stu_Data-Attributes/Unsolved/assets/js/script.js
+++ 20-Stu_Data-Attributes/Unsolved/assets/js/script.js
@@ -1,7 +1,24 @@
 var container = document.querySelector(".container");
 
-container.addEventListener("click", function(event) {
+container.addEventListener("click", function (event) {
   var element = event.target;
 
   // TODO: Complete function
-});
+  if (element.matches(".box")) {
+    var state = element.getAttribute("data-state");
+
+    var num = element.getAttribute("data-number");
+    if (state === "hidden") {
+      element.textContent = num;
+      element.setAttribute("data-state", "visible");
+    }
+
+    else {
+      element.textContent="";
+      element.setAttribute("data-state", "hidden");
+    }
+
+  }
+}
+);
+

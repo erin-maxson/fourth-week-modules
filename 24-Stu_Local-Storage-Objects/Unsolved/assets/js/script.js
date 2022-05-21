a19ebfaddd570f408abe5dac11a6bbfbca806a3f
erin-maxson <erin@cohatch.com>
2022-05-21 08:54:50 -0700

    string + parsing

--- 24-Stu_Local-Storage-Objects/Unsolved/assets/js/script.js
+++ 24-Stu_Local-Storage-Objects/Unsolved/assets/js/script.js
@@ -7,8 +7,22 @@ var signUpButton = document.querySelector("#sign-up");
 signUpButton.addEventListener("click", function(event) {
   event.preventDefault();
   
-  // TODO: Create user object from submissio
-
+  // TODO: Create user object from submission
+  var user = {
+    firstName: firstNameInput.value,
+    lastName: lastNameInput.value,
+    email: emailInput.value,
+    password: passwordInput.value,
+  }
   // TODO: Set new submission to local storage 
-  
+  localStorage.setItem("user", JSON.stringify(user));
+  renderMessage();
+
 });
+
+function renderMessage() {
+  var userInfo = JSON.parse(localStorage.getItem("user"));
+  if (userInfo !== null) {
+    document.querySelector(".message").textContent = userInfo.firstName + " " + userInfo.lastName + "'s Email & Password is" + " " + userInfo.email + " " + userInfo.password}
+  }
+
